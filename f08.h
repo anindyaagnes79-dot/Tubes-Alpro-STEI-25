c67e62ef014c17ed40218a4207b61b67e570e840
jekskuy <dzakiadnan18@gmail.com>
2026-05-25 13:16:17 +0700

    Remove unused include and comments from f08.h

--- f08.h
+++ f08.h
@@ -2,27 +2,9 @@
 #define F08_H
 
 #include "f07.h"
-#include "web_pages.h"
 
-/*
- * Menampilkan riwayat tab yang sedang aktif.
- * Sesuai spek, perintah 'history <X>' menampilkan X riwayat terakhir.
- * Jika X melebihi total history, tampilkan semua yang ada.
- */
 void f08_history(HistoryTab *h, int X);
-
-/*
- * Jalankan perintah BACK <X>.
- * Mundur sebanyak X langkah.
- * Return 1 jika berhasil, 0 jika gagal.
- */
 int f08_back_x(HistoryTab *h, int x);
-
-/*
- * Jalankan perintah FORWARD <X>.
- * Maju sebanyak X langkah.
- * Return 1 jika berhasil, 0 jika gagal.
- */
 int f08_forward_x(HistoryTab *h, int x);
 
 #endif
