be81be937f142016ffbb328a3a32c7d9ba2e3f0e
jekskuy <dzakiadnan18@gmail.com>
2026-05-25 13:15:37 +0700

    Refactor history management using stack data structure

--- f07.c
+++ f07.c
@@ -2,101 +2,71 @@
 #include <stdio.h>
 #include <string.h>
 
+void CreateEmptyStack(Stack *S) { 
+    S->top = -1; 
+}
+bool IsEmptyStack(Stack S) { 
+    return S.top == -1; 
+}
+bool IsFullStack(Stack S) { 
+    return S.top == MAX_HISTORY - 1; 
+}
+void Push(Stack *S, const char *url) {
+    if (!IsFullStack(*S)) {
+        S->top++;
+        strncpy(S->data[S->top], url, MAX_URL_LEN - 1);
+        S->data[S->top][MAX_URL_LEN - 1] = '\0';
+    }
+}
+void Pop(Stack *S, char *out_url) {
+    if (!IsEmptyStack(*S)) {
+        strcpy(out_url, S->data[S->top]);
+        S->top--;
+    }
+}
+
 void f07_init(HistoryTab *h) {
-    h->total            = 0;
-    h->posisi_sekarang  = -1; 
+    CreateEmptyStack(&h->backStack);
+    CreateEmptyStack(&h->forwardStack);
 }
 
 void f07_tambah_url(HistoryTab *h, const char *url) {
+    CreateEmptyStack(&h->forwardStack);
 
-    if (h->posisi_sekarang < h->total - 1) {
-        h->total = h->posisi_sekarang + 1;
-    }
-
-    if (h->total >= MAX_HISTORY) {
+    if (IsFullStack(h->backStack)) {
         printf("Peringatan: History sudah penuh, URL lama dihapus.\n");
-        int i;
-        for (i = 0; i < MAX_HISTORY - 1; i++) {
-            strncpy(h->urls[i], h->urls[i + 1], MAX_URL_LEN - 1);
-            h->urls[i][MAX_URL_LEN - 1] = '\0';
+        for (int i = 0; i < MAX_HISTORY - 1; i++) {
+            strcpy(h->backStack.data[i], h->backStack.data[i + 1]);
         }
-        h->total--;
-        h->posisi_sekarang--;
+        h->backStack.top--;
     }
-
-    strncpy(h->urls[h->total], url, MAX_URL_LEN - 1);
-    h->urls[h->total][MAX_URL_LEN - 1] = '\0';
-    h->total++;
-    h->posisi_sekarang = h->total - 1;
+    Push(&h->backStack, url);
 }
 
-static void buka_halaman_di_posisi(HistoryTab *h,
-                                   WebPageSet *pages,
-                                   CacheMap   *cache,
-                                   AdjMatrix  *adj)
-{
-    const char *url = h->urls[h->posisi_sekarang];
-
-    OpenPageContext ctx;
-    int cache_hit;
-
-    int ditemukan = f03_open_page(url, pages, cache, adj, &ctx, &cache_hit);
-
-    if (!ditemukan) {
-        printf("Halaman '%s' tidak ditemukan di database (mungkin sudah dihapus).\n\n", url);
-        return;
-    }
-
-    f03_display_page(&ctx, cache_hit);
-}
-
-int f07_back(HistoryTab *h,
-             WebPageSet *pages,
-             CacheMap   *cache,
-             AdjMatrix  *adj)
-{
-    if (h->posisi_sekarang < 0) {
+int f07_back(HistoryTab *h, WebPageSet *pages, CacheMap *cache, AdjMatrix *adj) {
+    if (h->backStack.top <= 0) {
         printf("ERROR: BACK TIDAK BISA DIJALANKAN KARENA TIDAK ADA HALAMAN SEBELUMNYA!\n\n");
         return 0;
     }
 
-    if (h->posisi_sekarang == 0) {
-        printf("ERROR: BACK TIDAK BISA DIGUNAKAN LAGI KARENA TIDAK ADA HALAMAN SEBELUMNYA!\n\n");
-        buka_halaman_di_posisi(h, pages, cache, adj);
-        return 0;
-    }
+    char temp[MAX_URL_LEN];
+    Pop(&h->backStack, temp);
+    Push(&h->forwardStack, temp); 
 
-    h->posisi_sekarang--;
-    printf("BACK: KEMBALI KE HALAMAN %s\n\n", h->urls[h->posisi_sekarang]);
-    buka_halaman_di_posisi(h, pages, cache, adj);
+    printf("BACK: KEMBALI KE HALAMAN %s\n\n", h->backStack.data[h->backStack.top]);
     return 1;
 }
 
-int f07_forward(HistoryTab *h,
-                WebPageSet *pages,
-                CacheMap   *cache,
-                AdjMatrix  *adj)
-{
-    if (h->posisi_sekarang < 0) {
+int f07_forward(HistoryTab *h, WebPageSet *pages, CacheMap *cache, AdjMatrix *adj) {
+    if (IsEmptyStack(h->forwardStack)) {
         printf("ERROR: FORWARD TIDAK BISA DIJALANKAN KARENA TIDAK ADA HALAMAN SELANJUTNYA!\n\n");
         return 0;
     }
 
-    if (h->posisi_sekarang >= h->total - 1) {
-        printf("ERROR: FORWARD TIDAK BISA DIJALANKAN KARENA TIDAK ADA HALAMAN SELANJUTNYA!\n\n");
-        buka_halaman_di_posisi(h, pages, cache, adj);
-        return 0;
-    }
+    char temp[MAX_URL_LEN];
+    Pop(&h->forwardStack, temp);
+    Push(&h->backStack, temp); 
 
-    h->posisi_sekarang++;
-    printf("FORWARD: KEMBALI KE HALAMAN %s\n\n", h->urls[h->posisi_sekarang]);
-    buka_halaman_di_posisi(h, pages, cache, adj);
+    printf("FORWARD: MAJU KE HALAMAN %s\n\n", h->backStack.data[h->backStack.top]);
     return 1;
-}
-
-const char *f07_url_sekarang(const HistoryTab *h) {
-    if (h->posisi_sekarang < 0 || h->total == 0) {
-        return NULL;
-    }
-    return h->urls[h->posisi_sekarang];
 }
