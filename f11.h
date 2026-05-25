608cabc29ca7acb9f1e5a03fc629f2669f5c0ff1
jekskuy <dzakiadnan18@gmail.com>
2026-05-25 12:48:37 +0700

    f11.h

---
+++ f11.h
@@ -0,0 +1,9 @@
+#ifndef F11_H
+#define F11_H
+
+#include "f00.h"
+void EksekusiSaveData(char *folderTarget, ListHalaman_Web *database);
+
+int JalankanExitManager(ListHalaman_Web *database);
+
+#endif
