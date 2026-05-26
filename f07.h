343827a12e1054e2d90b18c5281b526a6ec5f2f4
jekskuy <dzakiadnan18@gmail.com>
2026-05-26 12:28:12 +0700

    Refactor stack implementation for history management

--- f07.h
+++ f07.h
@@ -1,32 +1,62 @@
 #ifndef F07_H
 #define F07_H
 
-#include "f03.h"
-#include "f04.h"
-#include "web_pages.h"
-#include "adj_matrix.h"
 #include <stdbool.h>
 
 #define MAX_HISTORY 100
+#define MAX_URL_LEN 256
+
 typedef struct {
     char data[MAX_HISTORY][MAX_URL_LEN];
     int top; 
-} Stack;
+} StackHistory;
 
-void CreateEmptyStack(Stack *S);
-bool IsEmptyStack(Stack S);
-bool IsFullStack(Stack S);
-void Push(Stack *S, const char *url);
-void Pop(Stack *S, char *out_url);
+void CreateEmptyStack(StackHistory *S);
+bool IsEmptyStack(StackHistory S);
+bool IsFullStack(StackHistory S);
+void Push(StackHistory *S, const char *url);
+void Pop(StackHistory *S, char *out_url);
 
 typedef struct {
-    Stack backStack;   
-    Stack forwardStack; 
+    StackHistory backStack;   
+    StackHistory forwardStack; 
+} HistoryTab;
+
+
+void f07_init(HistoryTab *h);
+void f07_tambah_url(HistoryTab *h, const char *url);
+int f07_back(HistoryTab *h);
+int f07_forward(HistoryTab *h);
+
+#endif#ifndef F07_H
+#define F07_H
+
+#include <stdbool.h>
+
+#define MAX_HISTORY 100
+#define MAX_URL_LEN 256
+
+
+typedef struct {
+    char data[MAX_HISTORY][MAX_URL_LEN];
+    int top; 
+} StackHistory;
+
+
+void CreateEmptyStack(StackHistory *S);
+bool IsEmptyStack(StackHistory S);
+bool IsFullStack(StackHistory S);
+void Push(StackHistory *S, const char *url);
+void Pop(StackHistory *S, char *out_url);
+
+typedef struct {
+    StackHistory backStack;    
+    StackHistory forwardStack; 
 } HistoryTab;
 
 void f07_init(HistoryTab *h);
 void f07_tambah_url(HistoryTab *h, const char *url);
-int f07_back(HistoryTab *h, WebPageSet *pages, CacheMap *cache, AdjMatrix *adj);
-int f07_forward(HistoryTab *h, WebPageSet *pages, CacheMap *cache, AdjMatrix *adj);
+int f07_back(HistoryTab *h);
+int f07_forward(HistoryTab *h);
 
 #endif
