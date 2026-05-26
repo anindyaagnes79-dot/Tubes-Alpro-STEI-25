1aff07639f85a4821f7ed75bd8955d902cca0b7d
jekskuy <dzakiadnan18@gmail.com>
2026-05-26 12:29:21 +0700

    Refactor stack functions for StackHistory type

--- f07.c
+++ f07.c
@@ -2,28 +2,40 @@
 #include <stdio.h>
 #include <string.h>
 
-void CreateEmptyStack(Stack *S) { 
-    S->top = -1; 
+void CreateEmptyStack(StackHistory *S) {
+    S->top = -1;
 }
-bool IsEmptyStack(Stack S) { 
-    return S.top == -1; 
+
+bool IsEmptyStack(StackHistory S) {
+    return S.top == -1;
 }
-bool IsFullStack(Stack S) { 
-    return S.top == MAX_HISTORY - 1; 
+
+bool IsFullStack(StackHistory S) {
+    return S.top == MAX_HISTORY - 1;
 }
-void Push(Stack *S, const char *url) {
+
+void Push(StackHistory *S, const char *url) {
     if (!IsFullStack(*S)) {
         S->top++;
         strncpy(S->data[S->top], url, MAX_URL_LEN - 1);
         S->data[S->top][MAX_URL_LEN - 1] = '\0';
+    } else {
+
+        for (int i = 0; i < MAX_HISTORY - 1; i++) {
+            strcpy(S->data[i], S->data[i + 1]);
+        }
+        strncpy(S->data[S->top], url, MAX_URL_LEN - 1);
+        S->data[S->top][MAX_URL_LEN - 1] = '\0';
     }
 }
-void Pop(Stack *S, char *out_url) {
+
+void Pop(StackHistory *S, char *out_url) {
     if (!IsEmptyStack(*S)) {
         strcpy(out_url, S->data[S->top]);
         S->top--;
     }
 }
+
 
 void f07_init(HistoryTab *h) {
     CreateEmptyStack(&h->backStack);
@@ -31,42 +43,39 @@ void f07_init(HistoryTab *h) {
 }
 
 void f07_tambah_url(HistoryTab *h, const char *url) {
+
     CreateEmptyStack(&h->forwardStack);
 
-    if (IsFullStack(h->backStack)) {
-        printf("Peringatan: History sudah penuh, URL lama dihapus.\n");
-        for (int i = 0; i < MAX_HISTORY - 1; i++) {
-            strcpy(h->backStack.data[i], h->backStack.data[i + 1]);
-        }
-        h->backStack.top--;
-    }
     Push(&h->backStack, url);
 }
 
-int f07_back(HistoryTab *h, WebPageSet *pages, CacheMap *cache, AdjMatrix *adj) {
+
+int f07_back(HistoryTab *h) {
     if (h->backStack.top <= 0) {
-        printf("ERROR: BACK TIDAK BISA DIJALANKAN KARENA TIDAK ADA HALAMAN SEBELUMNYA!\n\n");
+        printf("Mundur gagal. Riwayat kosong atau sudah berada pada awal riwayat.\n\n");
         return 0;
     }
 
-    char temp[MAX_URL_LEN];
-    Pop(&h->backStack, temp);
-    Push(&h->forwardStack, temp); 
+    char current_url[MAX_URL_LEN];
 
-    printf("BACK: KEMBALI KE HALAMAN %s\n\n", h->backStack.data[h->backStack.top]);
+    Pop(&h->backStack, current_url);
+    Push(&h->forwardStack, current_url);
+
+    printf("Mundur ke halaman: %s\n\n", h->backStack.data[h->backStack.top]);
     return 1;
 }
 
-int f07_forward(HistoryTab *h, WebPageSet *pages, CacheMap *cache, AdjMatrix *adj) {
+int f07_forward(HistoryTab *h) {
     if (IsEmptyStack(h->forwardStack)) {
-        printf("ERROR: FORWARD TIDAK BISA DIJALANKAN KARENA TIDAK ADA HALAMAN SELANJUTNYA!\n\n");
+        printf("Maju gagal. Riwayat masa depan kosong.\n\n");
         return 0;
     }
 
-    char temp[MAX_URL_LEN];
-    Pop(&h->forwardStack, temp);
-    Push(&h->backStack, temp); 
+    char next_url[MAX_URL_LEN];
 
-    printf("FORWARD: MAJU KE HALAMAN %s\n\n", h->backStack.data[h->backStack.top]);
+    Pop(&h->forwardStack, next_url);
+    Push(&h->backStack, next_url);
+
+    printf("Maju ke halaman: %s\n\n", h->backStack.data[h->backStack.top]);
     return 1;
 }
