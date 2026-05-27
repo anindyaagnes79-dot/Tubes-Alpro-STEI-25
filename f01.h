8eb1df8e4b7bb149e387b63f4ceebb1e58655794
kiarayl <kiara.erlangga@gmail.com>
2026-05-27 23:38:25 +0700

    Add files via upload

--- f01.h
+++ f01.h
@@ -1,27 +1,51 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-#include <time.h>
+#ifndef F01
+#define F01
 
-#define MAX_capacity 100
+#define IDX_UNDEF -1
+#include "f00.h"
 
-typedef struct {
-    int id;
-    char* web_url;
-    char* content;
-} Halaman_Web;
+unsigned int AcakLCG(){
+    static unsigned int angka = 16525;
+    unsigned long long A=67676767;
+    unsigned long long C=1322518325;
+    unsigned long long M=414141414;
+    angka=(A*angka+C)%M;
+    return angka;
+}
+void Discover(ListHalaman_Web *l){
+    if(isEmpty(*l)==true){
+        printf ("ERROR, TIDAK ADA WEB DI DATABASE");
+        return;
+    }
+    else if ((l->nEff)<5){
+    	int i;
+        for (i=0; i<l->nEff; i++){
+            printf ("- %s", l->HW[i].web_url);
+            if (i < l->nEff - 1) {
+                printf(", ");
+            }
+        }
+    }
+    else{
+        int index_acak[MAX_WEB_PAGES];
+        int i;
+        for (i=0; i<l->nEff; i++){
+            index_acak[i]=i;
+        }
+        for (i=l->nEff-1; i > 0; i--) {
+            int j=AcakLCG() % (i + 1);
+            int temp=index_acak[i];
+            index_acak[i]=index_acak[j];
+            index_acak[j]=temp;
+        }
+        for (i = 0; i < 5; i++) {
+            printf("- %s", l->HW[index_acak[i]].web_url);
+            if (i < 4) {
+                printf(", ");
+            }
+        }
+    }
+    printf ("\n");
+}
 
-typedef struct {
-    Halaman_Web HW[MAX_capacity];
-    int nEff;
-} ListHalaman_Web;
-
-void BuatHalaman_Web(Halaman_Web *h, int id, char* web_url, char* content);
-void BuatListHalaman_Web(ListHalaman_Web *l);
-bool isEmpty(ListHalaman_Web l);
-bool isFull(ListHalaman_Web l);
-bool isIndexEff(ListHalaman_Web l, int i);
-int IndexTerbesar(ListHalaman_Web l);
-void TambahHalamanWeb(ListHalaman_Web *l, Halaman_Web h);
-unsigned int AcakLCG()
-void Discover(ListHalaman_Web *l)
+#endif
