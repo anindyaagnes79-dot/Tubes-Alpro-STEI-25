6660b5683b7ba669394d0347acab7788e4e82a27
kiarayl <kiara.erlangga@gmail.com>
2026-05-27 23:42:08 +0700

    Add files via upload

--- f11.h
+++ f11.h
@@ -2,6 +2,11 @@
 #define F11_H
 
 #include "f00.h"
+
+void MuatDataDariCSV(const char *pathFile, ListHalaman_Web *database);
+
+void MuatGrafDariCSV(const char *pathFile, ListHalaman_Web *database);
+
 void EksekusiSaveData(char *folderTarget, ListHalaman_Web *database);
 
 int JalankanExitManager(ListHalaman_Web *database);
